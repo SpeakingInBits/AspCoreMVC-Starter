0f36048ad3df8ba9c5d588d1f4197c9789285e84
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2021-05-05 19:31:33 -0700

    Extend default IdentityUser to allow for custom user data

---
+++ AspCoreMVC-Starter/Data/CustomIdentityUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreMVC_Starter.Data
+{
+    /// <summary>
+    /// Class used to extend existing Identity User
+    /// https://docs.microsoft.com/en-us/aspnet/core/security/authentication/customize-identity-model?view=aspnetcore-5.0#custom-user-data
+    /// </summary>
+    public class CustomIdentityUser : IdentityUser
+    {
+        // TODO: Add your custom user data here
+    }
+}
