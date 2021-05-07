7164c5f0faaddeea20433732da7822ae7ca6482c
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2021-05-06 21:09:25 -0700

    Add project to create default admin and roles

---
+++ InitDatabase/MyConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using AspCoreMVC_Starter.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitDatabase
+{
+    class MyConfiguration
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json", true, true);
+            var dbConnection = builder.Build().GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(dbConnection));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+        }
+    }
+}
---
+++ InitDatabase/Program.cs
@@ -0,0 +1,72 @@
+﻿using AspCoreMVC_Starter.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InitDatabase
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            MyConfiguration.ConfigureServices(serviceCollection);
+
+            using var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            ApplicationDbContext context = serviceProvider.GetService<ApplicationDbContext>();
+            UserManager<IdentityUser> userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+            RoleManager<IdentityRole> roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+
+            await RunPendingMigrations(context);
+            await CreateDefaultRoles(roleManager);
+            await CreateDefaultAdmin(userManager);
+        }
+
+        private static async Task CreateDefaultAdmin(UserManager<IdentityUser> userManager)
+        {
+            // TODO: Create your default admin. Ensure the password meets your requirements!
+            const string email = "admin@yourwebsite.com";
+            const string username = "Admin";
+            const string password = "Programming01#";
+
+            // Do nothing if admin is already created
+            if (await userManager.FindByEmailAsync(email) != null)
+            {
+                return;
+            }
+
+            var adminUser = new IdentityUser()
+            {
+                Email = email,
+                UserName = username,
+                EmailConfirmed = true
+            };
+
+            await userManager.CreateAsync(adminUser, password);
+            await userManager.AddToRoleAsync(adminUser, IdentityHelper.AdminRole);
+        }
+
+        private static async Task RunPendingMigrations(ApplicationDbContext context)
+        {
+            await context.Database.MigrateAsync();
+        }
+
+        private static async Task CreateDefaultRoles(RoleManager<IdentityRole> roleManager)
+        {
+            IEnumerable<string> roles = IdentityHelper.GetRoles();
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
+}
